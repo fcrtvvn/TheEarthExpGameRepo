7b10b93ec63d3b6d6b1283deb5f97428debfdd83
fcrtvvn <fortunemsomuadina44@gmail.com>
2022-04-21 03:14:04 +0100

    most recent

--- EarthExpGame/Assets/Scripts/AdManager.cs
+++ EarthExpGame/Assets/Scripts/AdManager.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
-public class AdManager : MonoBehaviour
+public class AdManager : MonoBehaviour, IUnityAdsListener
 {
     void Start()
     {
         Advertisement.Initialize("4718631");
     }
 
-    void Update()
+    public void PlayRewardedAd()
     {
-        
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            Advertisement.Show("rewardedVideo");
+        }
+        else
+        {
+            Debug.Log("Rewarded Ad not ready.");
+        }
+    }
+
+    public void OnUnityAdsReady(string placementID)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsDidStart(string placementID)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsDidFinish(string placementID, ShowResult showResult)
+    {
+        throw new System.NotImplementedException();
     }
 }
