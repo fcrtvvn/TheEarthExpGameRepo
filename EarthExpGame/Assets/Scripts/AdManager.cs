88be0c60d6f1d57699e9afc065e0761fa43339cf
fcrtvvn <fortunemsomuadina44@gmail.com>
2022-04-21 12:15:56 +0100

    recent

--- EarthExpGame/Assets/Scripts/AdManager.cs
+++ EarthExpGame/Assets/Scripts/AdManager.cs
@@ -1,46 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Advertisements;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Advertisements;
 
-//IUnityAdsListener
 
-public class AdManager : MonoBehaviour, I
-{
-    void Start()
-    {
-        Advertisement.Initialize("4718631");
-    }
+//public class AdManager : MonoBehaviour, IUnityAdsListener
+//{
+//    string placement = "rewardedVideo";
 
-    public void PlayRewardedAd()
-    {
-        if (Advertisement.IsReady("rewardedVideo"))
-        {
-            Advertisement.Show("rewardedVideo");
-        }
-        else
-        {
-            Debug.Log("Rewarded Ad not ready.");
-        }
-    }
+//    IEnumerator Start()
+//    {
+//        Advertisement.AddListener(this);
+//        Advertisement.Initialize("4718631", true);
 
-    public void OnUnityAdsReady(string placementID)
-    {
-        throw new System.NotImplementedException();
-    }
+//        while (!Advertisement.IsReady(placement))
+//            yield return null;
 
-    public void OnUnityAdsDidError(string message)
-    {
-        throw new System.NotImplementedException();
-    }
+//        Advertisement.Show(placement);
+//    }
 
-    public void OnUnityAdsDidStart(string placementID)
-    {
-        throw new System.NotImplementedException();
-    }
+//    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+//    {
+//        //throw new System.NotImplementedException();
+//        if (showResult == showResult.Finished)
+//        {
+//            SeedsManager.Totalseeds++ =;
+//        }
+//        else if (showResult == showResult.Failed)
+//        {
+//            //on no!
+//        }
+//    }
 
-    public void OnUnityAdsDidFinish(string placementID, ShowResult showResult)
-    {
-        throw new System.NotImplementedException();
-    }
-}
+//    public void OnUnityAdsDidError(string message)
+//    {
+//        //throw new System.NotImplementedException();
+//    }
+
+//    public void OnUnityAdsDidStart(string placementId)
+//    {
+//        //throw new System.NotImplementedException();
+//    }
+
+//    public void OnUnityAdsReady(string placementId)
+//    {
+//        //throw new System.NotImplementedException();
+//    }
+
+   
+
+
+//    //public interface IUnityAdsListener
+//    //{
+
+//    //}
+//    ////public static bool IsReady(string placementId);
+
+//    //void Start()
+//    //{
+//    //    Advertisement.Initialize("4718631");
+//    //}
+
+//    //public void PlayRewardedAd()
+//    //{
+//    //    if (Advertisement.IsReady("rewardedVideo"))
+//    //    {
+//    //        Advertisement.Show("rewardedVideo");
+//    //    }
+//    //    else
+//    //    {
+//    //        Debug.Log("Rewarded Ad not ready.");
+//    //    }
+//    //}
+
+    
+//}
