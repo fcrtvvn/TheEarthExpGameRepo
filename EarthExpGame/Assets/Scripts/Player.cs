7ef93ebdbbf3abef2382547048af86e1f35a71f0
fcrtvvn <fortunemsomuadina44@gmail.com>
2022-04-22 21:18:20 +0100

    Currently on save and load

---
+++ EarthExpGame/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int trees;
+    public int water;
+    public int seeds;
+    public int carbon;
+
+
+    
+}
